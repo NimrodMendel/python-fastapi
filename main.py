25bf09f6ec75ca0b6f61fd74fa7707a41f6a3bdd
NimrodMendel <mendel.nimrod@gmail.com>
2022-05-28 19:01:17 +0300

    Add schema validation using Pydantic package

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models import Post
 
 app = FastAPI()
 
@@ -11,3 +12,10 @@ async def root():
 @app.get('/posts')
 async def get_posts():
     return {"message": "No posts yet"}
+
+
+@app.post('/posts')
+async def add_post(new_post: Post):
+    if not new_post.is_published:
+        return {"title": new_post.title, "content": new_post.content}
+    return {"message": "Post already published"}
